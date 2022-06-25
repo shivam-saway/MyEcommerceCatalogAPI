fa1155a696a6ec4c0cb13b41f5bcd3355e2e8641
= <=>
2022-06-25 14:00:53 +0530

    added controller for microservices

---
+++ src/services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,80 @@
+﻿using Catalog.API.Entities;
+using Catalog.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CatalogController : ControllerBase
+    {
+        private readonly IProductRepository productRepository;
+        private readonly ILogger<CatalogController> logger;
+
+        public CatalogController(IProductRepository productRepository, ILogger<CatalogController> logger)
+        {
+            this.productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpGet("/get-products", Name = "GetProducts")]
+        [ProducesResponseType(StatusCodes.Status200OK,Type = typeof(IEnumerable<Product>))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await productRepository.GetProducts();
+            if(products == null)
+            {
+                logger.LogError("Products Not Found");
+                return NotFound();
+            }
+            return Ok(products);
+        }
+        [HttpGet("{id:length(24)}", Name = "GetProduct")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Product))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<Product>> GetProductsById(string id)
+        {
+            var product = await productRepository.GetProduct(id);
+            if(product == null)
+            {
+                logger.LogError($"Product with id - {id} not found.");
+                return NotFound();
+            }
+            return Ok(product);
+        }
+        [Route("[action]/{category}", Name = "GetProductByCategory")]
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<Product>))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProductByCategory(string category)
+        {
+            var products = await productRepository.GetProductByCategory(category);
+            if(products == null)
+            {
+                logger.LogError("Not Found");
+                return NotFound();
+            }
+            return Ok(products);
+        }
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Product))]
+        public async Task<ActionResult<Product>> CreateProduct([FromBody] Product product)
+        {
+            await productRepository.CreateProduct(product);
+            return CreatedAtRoute("GetProduct", new { id = product.Id }, product);
+        }
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Product))]
+        public async Task<IActionResult> UpdateProduct([FromBody] Product product)
+        {
+            return Ok(await productRepository.UpdateProduct(product));
+        }
+        [HttpDelete("{id:length(24)}", Name = "DeleteProduct")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Product))]
+        public async Task<IActionResult> DeleteProductById(string id)
+        {
+            return Ok(await productRepository.DeleteProduct(id));
+        }
+    }
+}
--- src/services/Catalog/Catalog.API/Data/CatalogContext.cs
+++ src/services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -7,12 +7,13 @@ namespace Catalog.API.Data
     {
         private readonly IConfiguration configuration;
 
-        public CatalogContext(IConfiguration configuration)
+        public CatalogContext(IConfiguration configuration, CatalogContextSeed catalogContextSeed)
         {
             this.configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
             var client = new MongoClient(this.configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
             var database = client.GetDatabase(this.configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
             this.Products = database.GetCollection<Product>(this.configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            CatalogContextSeed.Seed(Products);
         }
         public IMongoCollection<Product> Products { get; }
     }
