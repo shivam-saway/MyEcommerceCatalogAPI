f508fb6f4d9189de096564170321ab558470330b
= <=>
2022-07-21 15:46:49 +0530

    completed basket api microservice

---
+++ src/services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,49 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Basket.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository basketRepository;
+
+        public BasketController(IBasketRepository basketRepository)
+        {
+            this.basketRepository = basketRepository ?? throw new ArgumentNullException(nameof(basketRepository));
+        }
+
+        [HttpGet("{userName}", Name = "GetBasket")]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+        {
+            var basket = await basketRepository.GetBasket(userName);
+            return Ok(basket ?? new ShoppingCart(userName));
+        }
+        [HttpPost]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
+        {
+            // TODO : Communicate with Discount.Grpc
+            // and Calculate latest prices of product into shopping cart
+            // consume Discount Grpc
+            //foreach (var item in basket.Items)
+            //{
+            //    var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
+            //    item.Price -= coupon.Amount;
+            //}
+
+            return Ok(await basketRepository.UpdateBasket(basket));
+        }
+        [HttpDelete("{userName}", Name = "DeleteBasket")]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteBasket(string userName)
+        {
+            await basketRepository.DeleteBasket(userName);
+            return Ok();
+        }
+    }
+}
---
+++ src/services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,37 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache distributedCache;
+
+        public BasketRepository( IDistributedCache distributedCache )
+        {
+            this.distributedCache = distributedCache ?? throw new ArgumentNullException(nameof(distributedCache));
+        }
+        public async Task<ShoppingCart?> GetBasket(string userName)
+        {
+            var basket = await distributedCache.GetStringAsync(userName);
+
+            if (String.IsNullOrEmpty(basket))
+                return null;
+
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart?> UpdateBasket(ShoppingCart basket)
+        {
+            await distributedCache.SetStringAsync(basket.UserName, JsonConvert.SerializeObject(basket));
+
+            return await GetBasket(basket.UserName ?? "");
+        }
+
+        public async Task DeleteBasket(string userName)
+        {
+            await distributedCache.RemoveAsync(userName);
+        }
+    }
+}
