750cae48cdf9c138c6909f21825666d82d00cc8c
= <=>
2022-08-15 15:36:54 +0530

    added discount api with containerization support

---
+++ src/services/Discount/Discount.API/Program.cs
@@ -0,0 +1,32 @@
+using Discount.API.Extensions;
+using Discount.API.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Discount.API", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(options => options.SwaggerEndpoint("/swagger/v1/swagger.json", "Discount.API v1"));
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MigrateDatabase<Program>();
+
+app.Run();
